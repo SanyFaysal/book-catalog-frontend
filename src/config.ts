d1e4ae8fefd49ce5c9647b0d691c5ad1bc56f033
SanyFaysal <tfoysalahmed@gmail.com>
2023-12-31 13:51:41 +0600

    [UPDATEW]: server url

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,4 @@ const node_environment = process.env.NODE_ENV;
 export const server_url =
     node_environment === "development"
         ? "http://localhost:8080/api/v1"
-        : "https://job-portal-server-b7p7.onrender.com/api/v1";
+        : "https://book-catalog-backend-9jhf9gsel-sanyfaysal.vercel.app/api/v1";
